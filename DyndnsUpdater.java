bfc603ab6dabc9a9a1a6205fbc8def5f8fcd91aa
Lorenzo De Luca <lorenzo@deluca.pro>
2023-06-29 13:47:16 +0200

    project uploaded

---
+++ DyndnsUpdater.java
@@ -0,0 +1,150 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * @author Lorenzo De Luca
+ */
+public class DyndnsUpdater{
+    static File ipFile = new File("ip.txt");
+    static String dns_zone_name="x.pro";
+    static long dns_record_id =  1234567890L;
+    static String dns_record_subdomain =  "y";
+    static String dns_record_target =  "";
+    static int dns_record_ttl =  3600;
+    static String ovh_endpoint =  "https://eu.api.ovh.com/1.0";
+    static String ovh_application_key = "123";
+    static String ovh_application_secret = "123";
+    static String ovh_consumer_key =  "123";
+
+    /*
+     * alternatives api to retrieve the public ip :
+     *  - https://ipv4.icanhazip.com/
+     *  - http://myexternalip.com/raw
+     *  - http://ipecho.net/plain
+     */
+    public static void dynDnsUpdate() throws IOException, NoSuchAlgorithmException{
+        FileWriter fw;
+        Scanner fr;
+        String oldIp;
+        String currentIp;
+
+        if(ipFile.createNewFile()){//create a new file if it doesnt exists yet
+            fw = new FileWriter(ipFile,false);
+            fw.write("x.x.x.x");
+            fw.close();
+        }
+
+        //reading the old ip from the db file
+        fr=new Scanner(ipFile);
+        oldIp=fr.nextLine();
+        fr.close();
+
+        //get current ip
+        String api = "http://checkip.amazonaws.com/";
+        URL urlApiPublicIp = new URL(api);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(urlApiPublicIp.openStream()))) {
+            currentIp = br.readLine();
+        }
+
+        if(!oldIp.equals(currentIp)){
+            dns_record_target=currentIp;
+            String urlUpdateDns="/domain/zone/"+dns_zone_name+"/dynHost/record/"+dns_record_id;
+            URL urlApiOvh = new URL(new StringBuilder(ovh_endpoint_url).append(urlUpdateDns).toString());
+            HttpURLConnection request = (HttpURLConnection) urlApiOvh.openConnection();
+            request.setRequestMethod("PUT");
+			request.setReadTimeout(30000);
+			request.setConnectTimeout(30000);
+			request.setRequestProperty("Content-Type", "application/json");
+			request.setRequestProperty("X-Ovh-Application", ovh_application_key);
+            long timestamp = System.currentTimeMillis() / 1000;
+            String body="{"+
+                            "\"ip\":\""+dns_record_target+"\","+
+                            "\"subDomain\":\""+dns_record_subdomain+"\""+"}";
+            String toSign = new StringBuilder(ovh_application_secret)
+									.append("+")
+									.append(ovh_consumer_key)
+									.append("+")
+									.append("PUT")
+									.append("+")
+									.append(urlApiOvh)
+									.append("+")
+									.append(body)
+									.append("+")
+									.append(timestamp)
+									.toString();
+            String signature = new StringBuilder("$1$").append(HashSHA1(toSign)).toString();
+            request.setRequestProperty("X-Ovh-Consumer", ovh_consumer_key);
+			request.setRequestProperty("X-Ovh-Signature", signature);
+			request.setRequestProperty("X-Ovh-Timestamp", Long.toString(timestamp));
+            request.setDoOutput(true);
+            DataOutputStream out = new DataOutputStream(request.getOutputStream());
+            out.writeBytes(body);
+            out.flush();
+            out.close();
+            String inputLine;
+			BufferedReader in;
+			int responseCode = request.getResponseCode();
+            System.out.println(Integer.toString(responseCode)+"-");
+			if (responseCode == 200) {
+				in = new BufferedReader(new InputStreamReader(request.getInputStream()));
+			} else {
+				in = new BufferedReader(new InputStreamReader(request.getErrorStream()));
+			}
+			StringBuilder response = new StringBuilder();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+            in.close();
+			System.out.println(response.toString());
+
+            //update ip.txt
+            fw = new FileWriter(ipFile,false);
+            fw.write(dns_record_target);
+            fw.close();
+        }
+    }
+
+    public static String HashSHA1(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	    MessageDigest md;
+        md = MessageDigest.getInstance("SHA-1");
+        byte[] sha1hash = new byte[40];
+        md.update(text.getBytes("iso-8859-1"), 0, text.length());
+        sha1hash = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < sha1hash.length; i++) {
+            sb.append(Integer.toString((sha1hash[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+	}
+    
+    public static void main(String[] args) {
+        int timeoutMinutes=30;
+        final ScheduledExecutorService updateScheduler = Executors.newSingleThreadScheduledExecutor();
+
+        updateScheduler.scheduleAtFixedRate(new Runnable(){
+            @Override
+            public void run(){
+                try {
+                    dynDnsUpdate();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, timeoutMinutes, TimeUnit.MINUTES);
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# dyndnsUpdaterJava
+A script to connect and auth in the ovh api and update a dynamic dns every time the public ip of the host change 
+
+to get ovh api key token: https://api.ovh.com/createToken/index.cgi?PUT=/*
+
+ovh api info: https://api.ovh.com/console/#/domain
